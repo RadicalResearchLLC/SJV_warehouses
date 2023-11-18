937cfa6b0c33c64bed815dedf2083c056046a2c2
Michael McCarthy <mikem@radicalresearch.llc>
2023-11-17 17:21:34 -0800

    updated to v1.01 with merged dataset and cities outlined

--- 1_Acquire.R
+++ 1_Acquire.R
@@ -1,9 +1,7 @@
 ##SJV Warehouse Map App V1
 ##Created by Mike McCarthy, Radical Research LLC
-##Inspired by Graham Brady and Susan Phillips at Pitzer College and their code
-##located here: https://docs.google.com/document/d/16Op4GgmK0A_0mUHAf9qqXzT_aekbdLb_ZFtBaZKfj6w/edit
-##First created May, 2022
-##Last modified February, 2023
+##First created February 2023
+##Last modified November 2023
 ##This script acquires and tidy parcel data for the app
 
 rm(list =ls()) # clear environment
@@ -37,18 +35,51 @@ tracy_dir <- paste0(wd, '/TracyZoning')
 
 #calEJScreen_dir <- paste0(wd, '/calenviroscreen40')
 
-
-
 gc()
 ##Set minimum size for analysis in thousand sq.ft. for non-warehouse classified
 sq_ft_threshold_WH <- 28000
 sq_ft_threshold_maybeWH <- 150000
 
-##Try to import LA County data
+## Import Stanislaus County data - 
+##https://open-data-stancounty-gis.hub.arcgis.com/datasets/stancounty-gis::parcels-for-download/about
+## pulled September 2023
+
+stanislausParcels <- sf::st_read(dsn = 'StanislausParcels.geojson') |> 
+  st_transform(crs = 4326) |>
+  st_zm(drop = TRUE, what = 'ZM')
+  #filter(SHAPE_Area >= sq_ft_threshold_WH)
+
+stanislaus_zoning <- sf::st_read(dsn = 'Zoning_AGOL_stanislaus.geojson') |> 
+  st_transform(crs = 4326)
+
+zoning_types_stsls <- stanislaus_zoning |> 
+  st_set_geometry(value = NULL) |> 
+  select(Zone_Descr, Zone_Label, Shape__Area) |> 
+  group_by(Zone_Descr, Zone_Label) |> 
+  summarize(count = n(), area = sum(Shape__Area))
+
+plannedZones_stsls <- stanislaus_zoning |> 
+  filter(Zone_Descr %in% c('Planned Industrial')) |> 
+  mutate(Zone_Label == 'P-I') 
+
+IndZones_stsls <- stanislaus_zoning |> 
+  filter(Zone_Label %in% c('M', 'L-M', 'SCP-IBP',
+                           'SCP-PI')) |> 
+  bind_rows(plannedZones_stsls)
+
+cities1 <- stanislaus_zoning |> 
+  filter(Zone_Label %in% c('CERES', 'HUGHSON', 'MODESTO',
+                           'NEWMAN', 'OAKDALE', 'PATTERSON',
+                           'RIVERBANK', 'TURLOCK', 'WATERFORD')) |> 
+  select(Zone_Descr, Zone_Label, geometry)
+
+zonePal <- colorFactor(palette = 'Dark2', domain = IndZones_stsls$Zone_Descr)
+
+##Try to import SJV County data
 sf::st_layers(dsn = parcel_dir)
 sjv_parcels <- sf::st_read(dsn = parcel_dir, quiet = TRUE, type = 3) %>%
-  st_transform("+proj=longlat +ellps=WGS84 +datum=WGS84") %>% 
-  filter(SHAPE_AREA >= sq_ft_threshold_maybeWH) %>% 
+  st_transform("+proj=longlat +ellps=WGS84 +datum=WGS84") |> 
+  filter(SHAPE_AREA >= sq_ft_threshold_maybeWH) |> 
   st_make_valid()
 
 sjv_apn <- sf::st_read(dsn = APNIndex_dir, quiet = TRUE, type = 3) %>%
@@ -56,82 +87,157 @@ sjv_apn <- sf::st_read(dsn = APNIndex_dir, quiet = TRUE, type = 3) %>%
 sjv_zoning <- sf::st_read(dsn = zoning_dir, quiet = TRUE, type = 3) %>%
   st_transform("+proj=longlat +ellps=WGS84 +datum=WGS84") 
 
-cities <- c('LATHROP', 'STOCKTON', 'TRACY', 'MANTECA',
+city_names <- c('LATHROP', 'STOCKTON', 'TRACY', 'MANTECA',
             'RIPON')
 
-zones <- sjv_zoning %>% 
-  st_set_geometry(value = NULL) %>% 
-  group_by(ZOCOLABEL, ZNDESCRIPT) %>% 
-  summarize(count = n(), .groups = 'drop') %>% 
-  mutate(WH = str_detect(ZNDESCRIPT, 'WAREHOUSE')) %>% 
-  mutate(Ind = str_detect(ZNDESCRIPT, 'INDUSTRIAL')) %>% 
-  mutate(truck = str_detect(ZNDESCRIPT, 'TRUCK')) %>% 
-  mutate(ag = str_detect(ZNDESCRIPT, 'AGRICULTURE')) %>% 
-  mutate(pubF = str_detect(ZNDESCRIPT, 'PUBLIC FACILITIES')) %>% 
-  mutate(sum = WH+truck+Ind + pubF) %>% 
-  filter(ag == FALSE) %>% 
-  filter(ZNDESCRIPT %in% cities | sum > 0) %>% 
-  filter(ZNDESCRIPT != 'GENERAL INDUSTRIAL') %>% 
-  filter(ZNDESCRIPT != 'TRACY') %>% 
-  left_join(sjv_zoning) %>% 
-  st_as_sf() %>% 
+zones <- sjv_zoning |> 
+  st_set_geometry(value = NULL) |> 
+  group_by(ZOCOLABEL, ZNDESCRIPT)  |> 
+  summarize(count = n(), .groups = 'drop') |> 
+  mutate(WH = str_detect(ZNDESCRIPT, 'WAREHOUSE')) |> 
+  mutate(Ind = str_detect(ZNDESCRIPT, 'INDUSTRIAL')) |> 
+  mutate(truck = str_detect(ZNDESCRIPT, 'TRUCK')) |> 
+  mutate(ag = str_detect(ZNDESCRIPT, 'AGRICULTURE')) |> 
+  mutate(pubF = str_detect(ZNDESCRIPT, 'PUBLIC FACILITIES')) |> 
+  mutate(sum = WH+truck+Ind) |> 
+  filter(ag == FALSE) |> 
+  filter( sum > 0) |> 
+ # filter(ZNDESCRIPT != 'GENERAL INDUSTRIAL') |> 
+ # filter(ZNDESCRIPT != 'TRACY') |> 
+  left_join(sjv_zoning) |> 
+  st_as_sf() |> 
   st_make_valid()
 
+zones1 <- colorFactor(palette = 'Set2', domain = zones$ZNDESCRIPT)
 
-whZ <- zones %>% 
-  st_join(sjv_parcels) %>% 
-  st_set_geometry(value = NULL) %>% 
-  left_join(sjv_parcels) %>% 
-  st_as_sf()
+sf_use_s2(FALSE)
 
-leaflet() %>% 
-  addTiles() %>% 
-  addProviderTiles(provider = providers$CartoDB.Positron,
-                   group = 'Basemap') %>% 
-  addProviderTiles(provider = providers$Esri.WorldImagery,
-                   group = 'Imagery') %>% 
-  addLayersControl(baseGroups = c('BaseMap', 'Imagery')) %>% 
-  addPolygons(data = whZ,
-              label = ~htmlEscape(paste(ZNDESCRIPT)),
-              weight = 1, 
-              fillOpacity = 0.4,
-              color= 'red')
+cities2 <- sjv_zoning |> 
+  filter(ZNDESCRIPT %in% city_names) |> 
+  select(ZNCODE, ZNLABEL, geometry) |> 
+  rename(Zone_Descr = ZNCODE, Zone_Label = ZNLABEL) |> 
+  bind_rows(cities1)
+
+whZ <-sjv_parcels |>
+  st_intersection(zones) |> 
+  select(APN_CHR, ZNDESCRIPT, geometry) |> 
+  mutate(category = 'Existing Industrial') |> 
+  rename(class = ZNDESCRIPT,
+         APN = APN_CHR) |> 
+  st_transform(crs= 4326)#|> 
+  #filter()
+
+types <- st_geometry_type(whZ)
+types_df <- data.frame(types)
+str(types_df)
+whZ_2 <- bind_cols(whZ, types_df)
+
+whZ_2 <- whZ |> 
+  filter(types %in% c('POLYGON', 'MULTIPOLYGON'
+                      #, 'GEOMETRYCOLLECTION' 
+                      )) 
+
+#leaflet() |> 
+ # addTiles() |> 
+  #addPolygons(data = whZ_2)
+
+  #st_set_geometry(value = NULL) |> 
+  #left_join(sjv_parcels) |> 
+  #st_as_sf()
 
 gc()
 
-tracy_zones <- sf::st_read(dsn = tracy_dir, layer = 'TracyZoning') %>% 
-  st_transform(crs = 4326) %>% 
+tracy_zones <- sf::st_read(dsn = tracy_dir, layer = 'TracyZoning') |> 
+  st_transform(crs = 4326) |> 
   filter(ZONING_COD %in% c('M-1', 'M-2', 'NEI', 'CRSP'))
 
-tracy_zone_codes <- tracy_zones %>% 
-  st_set_geometry(value = NULL) %>% 
-  group_by(ZONING_TYP, ZONING_COD) %>% 
+tracy_zone_codes <- tracy_zones |> 
+  st_set_geometry(value = NULL) |> 
+  group_by(ZONING_TYP, ZONING_COD) |> 
   summarize(count = n())
 
-tracy_parcels <- tracy_zones %>% 
-  st_join(sjv_parcels) %>% 
-  st_set_geometry(value = NULL) %>% 
-  left_join(sjv_parcels) %>% 
-  st_as_sf()
+tracy_parcels <-sjv_parcels |>
+  st_intersection(tracy_zones) |>
+  select(APN_CHR, ZONING_COD, geometry) |> 
+  mutate(category = 'Existing Industrial') |> 
+  rename(class = ZONING_COD,
+         APN = APN_CHR) |> 
+  st_transform(crs = 4326)
+  #st_set_geometry(value = NULL) |> 
+  #left_join(sjv_parcels) |> 
+  #st_as_sf()
 
+source('CEQA_plannedWH.R')
 
+uninc_Industrial_stsls <- stanislausParcels |> 
+  st_intersection(IndZones_stsls)  |>
+  select(APN, Zone_Descr) |> 
+  mutate(category = 'Existing Industrial') |> 
+  rename(class = Zone_Descr) |> 
+  st_transform(crs = 4326)
+  
+#  st_filter(stanislausParcels)
 
-leaflet() %>% 
-  addTiles() %>% 
+names(tracy_parcels)
+names(uninc_Industrial_stsls)
+names(whZ_2)
+
+CEQA_WH2 <- CEQA_WH |> 
+  mutate(category = 'Planned or approved',
+         class = 'TBD') |> 
+  rename(APN = ProjectName,
+         geometry = geom) |> 
+  st_transform(crs = 4326)
+
+str(tracy_parcels)
+str(CEQA_WH2)
+str(uninc_Industrial_stsls)
+str(whZ_2)
+
+leaflet() |> 
+  addTiles() |> 
+  addPolygons(data = tracy_parcels) |> 
+  addPolygons(data = CEQA_WH2) |> 
+  addPolygons(data =uninc_Industrial_stsls) |> 
+  addPolygons(data = whZ_2)
+
+centralWH <- bind_rows(tracy_parcels, uninc_Industrial_stsls, whZ_2, CEQA_WH2) 
+
+whPal <- colorFactor(palette = c('red', 'black'),
+                     domain = centralWH$category)
+
+cities2 <- cities2 |> 
+  filter(Zone_Label != 'TRACY')
+
+leaflet() |> 
   addProviderTiles(provider = providers$CartoDB.Positron,
-                   group = 'Basemap') %>% 
+                   group = 'Basemap') |> 
   addProviderTiles(provider = providers$Esri.WorldImagery,
-                   group = 'Imagery') %>% 
-  addLayersControl(baseGroups = c('BaseMap', 'Imagery')) %>% 
-  #addPolygons(data = whZ,
-  #            label = ~htmlEscape(paste(APN, ZNDESCRIPT)),
-  #            weight = 1, 
-  #            fillOpacity = 0.4,
-  #            color= 'red') %>% 
-  addPolygons(data = tracy_zones,
-              label = ~htmlEscape(ZONING_COD))
+                   group = 'Imagery') |> 
+  addLayersControl(baseGroups = c('BaseMap', 'Imagery')) |> 
+  addPolygons(data = centralWH,
+              color = ~whPal(category),
+              fillOpacity = 0.7,
+              weight = 1) |> 
+  addLegend(data = centralWH,
+            pal = whPal,
+            values = ~category,
+            title = 'Category') |> 
+  addPolygons(data =cities2,
+              color = 'lightblue',
+              fillOpacity = 0.3,
+              weight = 1) |> 
+  addLegend(data = cities2,
+            colors = 'lightblue',
+            labels = 'Cities - in progress')
 
 
+rm(ls = IndZones_stsls, sjv_parcels, sjv_apn, sjv_zoning, stanislaus_zoning, stanislausParcels,
+   tracy_zone_codes, tracy_zones)
+rm(ls = CentralValleyPlanned, cities1, whZ, zones, zoning_types_stsls, uninc_Industrial_stsls,
+   tracy_parcels, CEQA_WH, CEQA_WH2, plannedZones_stsls)
+
+gc()
 setwd(app_dir)
 save.image('.RData')
 
--- SanJoaquinWarehouses/README.md
+++ SanJoaquinWarehouses/README.md
@@ -1,22 +1,41 @@
-# RNOW San Joaquin County Warehouse Map - Prototype
+# Warehouse Central - v1.01
 
 ## Introduction
 
-The [Riverside Neighbors Opposing Warehouses (RNOW)](https://sites.google.com/view/rivnow/) Map of Warehouses in San Joaquin County is a publicly available tool developed to help visualize the development of warehouses. This dashboard is largely based off of the [Warehouse CITY](https://radicalresearch.shinyapps.io/WarehouseCITY/) tool developed in collaboration by [Redford Conservancy at Pitzer College](https://www.pitzer.edu/redfordconservancy/) and [Radical Research LLC](http://radicalresearch.llc). That tool allows additional quantification capabilities. The RNOW community group opposes new warehouse development around the Orangecrest and Mission Grove neighborhoods of the City of Riverside. 
+The Warehouse CENTRAL map of Warehouses in San Joaquin County is a publicly available tool developed to help visualize the development of warehouses in the Central Valley of California. This dashboard is largely based off of the [Warehouse CITY](https://radicalresearch.shinyapps.io/WarehouseCITY/) tool developed in collaboration by [Redford Conservancy at Pitzer College](https://www.pitzer.edu/redfordconservancy/), Riverside Neighbors Opposing Warehouses(https://sites.google.com/view/rivnow/) and [Radical Research LLC](http://radicalresearch.llc). That tool allows additional quantification capabilities. 
 
 ## Land-use (more like warehouse) Authorities
 
-Multiple land-use authorities contribute to the warehouses in the San Joaquin County. They include the cities of Stockton, Tracy, Manteca, Ripon, Lathrop, and San Joaquin County.  
+Multiple land-use authorities contribute to the warehouses in the Central Valley. They include the cities of Stockton, Tracy, Manteca, Ripon, Lathrop, and San Joaquin County.  
 
 ## Map and Data Provenance
 
 The map can be navigated using point, click, and drag features or by clicking on the zoom plus and minus buttons on the top left corner of the map. At the top right of the map, the imagery can be switched between a street level **Basemap** and satellite **Imagery**. 
 
--   **Existing warehouses**: Data processed using methodology established in Warehouse City tool v1.09. Raw data from [San Joaquin County GIS data](https://sjmap.org/GISDataDownload.htm).
-
+-   **Existing warehouses**: 
+Raw data from [San Joaquin County GIS data](https://sjmap.org/GISDataDownload.htm) and 
+[Stanislaus County Open Data] https://experience.arcgis.com/experience/29a526aa3639499f96b63f17c2b14cc5
 
 ## Contact and Support
 
 -   If you have questions or suggestions, please email [mikem@radicalresearch.llc](mailto:mikem@radicalresearch.llc){.email}
 
 -   If you are interested in supporting or becoming a member of local organizations opposing warehouse development adjacent to residential communities, please check out [RNOW](https://tinyurl.com/RIVNOW), Center for Community Action and Environmental Justice [(CCAEJ)](https://www.ccaej.org/), and the [Redford Conservancy at Pitzer College](https://www.pitzer.edu/redfordconservancy/).
+
+### Stanislaus County data disclaimer.  
+
+GIS data for Stanislaus county supplied by Stanislaus County.  As per the terms of use of that dataset, the following disclaimer is reproduced in its entirety.  
+
+<font size='1'>By using the Stanislaus County web page and accessing the Geographic Information Systems (GIS) data, you as the party requesting the data (hereafter the “Requestor”), assume all risks associated with the use of this site including any risk to your computer, software or data of being damaged by any virus, software, or any other file, which might be transmitted or activated via County’s web page or your access to it.
+
+Stanislaus County makes no warranty, expressed or implied, or representation as to the accuracy, timeliness or completeness of any of the data. Maps and (GIS) data are provided for illustration and informational purposes only and are not suitable for legal, engineering, or surveying purposes. While every effort has been made to ensure that the data is accurate, the maps and GIS data do not represent a legal survey and are not to be relied upon for any purpose whatsoever. Maps based on the GIS data are not legal documents. 
+
+Requestor is admonished that errors and omissions are likely present in the maps and GIS data. The GIS data is provided “as is” and with all faults and Requestor is strongly admonished to verify any data upon which Requestor intends to rely and to review or consult other sources. 
+
+Requestor by accessing the GIS data agrees to forever waive any and all rights, claims, causes of action or other recourse that it might otherwise have against the County of Stanislaus and that County of Stanislaus shall not be liable to Requestor or any other for any injuries or damages of any type, whether direct, indirect, incidental, consequential or otherwise, resulting from any error or omission in the data or in any programming used to obtain the data, by the inadequacy of the data obtained, by any error or omission of County of Stanislaus or Requestor’s systems, by any delay or failure to provide service, or by any other interruption, disruption or loss to Requestor’s operations or in any manner arising out of or related to the data retrieved hereunder. Requestor is solely responsible for any and all consequences of use of the data provided.  
+Requestor agrees to indemnify, defend, and hold Stanislaus County harmless for any and all liability of any nature arising out of or resulting from the Requestor’s access of the data, or the use of the data. 
+
+Any maps, publications, reports or other documents produced that utilize this digital data will credit the County of Stanislaus and the source of the data, such as “GIS data supplied by Stanislaus County” and shall attach a duplicate of this disclaimer.
+
+The Requestor acknowledges that he/she has read this disclaimer and understands it and is bound by its terms and conditions.</font>
+
