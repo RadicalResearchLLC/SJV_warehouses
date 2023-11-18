6df665aac2d3c325c8bd6a1a10f20018e9dd8864
Michael McCarthy <mikem@radicalresearch.llc>
2023-11-17 17:20:09 -0800

    updated to v1.01- merging all industrial zone parcels for a first cut of the project area.

--- SanJoaquinWarehouses/app.R
+++ SanJoaquinWarehouses/app.R
@@ -8,20 +8,28 @@
 #
 
 library(shiny)
-library(htmltools)
+#library(htmltools)
 library(leaflet)
 library(sf)
+
+deploy_date <- 'November 17, 2023'
+version <- 'Warehouse CENTRAL v1.01, last updated'
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
   titlePanel(
-    fluidRow(column(2, shiny::img(height = 80, src = 'RNOW.jpg')),
-             column(4,
-                    div(style = 'height:40px; font-size: 30px;',
-                        'San Joaquin County Warehouses - Prototype')),
-             column(2, shiny::img(height = 80, src = 'RNOW.jpg')))
+    titlePanel(
+      fluidRow(column(1),
+               column(3,
+                      div(style = 'height:60px; font-size: 30px;',
+                          'Warehouse CENTRAL')),
+               column(3, shiny::img(height = 60, src = 'Logo_Redford.jpg')),
+              # column(2, shiny::img(height = 38, src = 'Logo.png')),
+               column(3, shiny::img(height = 60, src = 'RNOW.png'))
+      )
+    ),
   ),
 
   tabsetPanel(
@@ -29,6 +37,13 @@ ui <- fluidPage(
              # Display slider bar selections, checkbox, and summary text
              fluidRow(
                column(width = 10, align = 'center', leafletOutput("map", height = 700))
+             ),
+             fluidRow(
+               column(width = 4, align = 'left',
+                      hr(),
+                      h3('Version and Deployment info'),
+                      hr(),
+                      paste(version, deploy_date))
              )),
     tabPanel('Readme',
              div(style = 'width: 90%; margin: auto;',
@@ -42,44 +57,42 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-palJuris2 <- colorFactor(palette = 'Paired', 
-                           domain = whZ$ZNDESCRIPT
+palWH <- colorFactor(palette = c('red', 'black'), 
+                           domain = centralWH$category
                          )
-palTracy <- colorFactor(palette = 'Set2',
-                        domain = tracy_parcels$ZONING_COD)
+
   
   output$map <- renderLeaflet({
-    leaflet() %>% 
-      addTiles() %>% 
+    leaflet() |> 
+      addTiles() |> 
       addProviderTiles(provider = providers$CartoDB.Positron,
-                       group = 'Basemap') %>% 
+                       group = 'Basemap') |> 
       addProviderTiles(provider = providers$Esri.WorldImagery,
-                       group = 'Imagery') %>% 
+                       group = 'Imagery') |> 
       addLayersControl(baseGroups = c('BaseMap', 'Imagery'),
-                       overlayGroups = c('SJV County', 'Tracy'),
-                       options = layersControlOptions(collapsed = FALSE)) %>% 
-      addPolygons(data = whZ,
-                  label = ~htmlEscape(paste(APN, ZNDESCRIPT)),
+                       overlayGroups = c('Industrial Parcels', 'Cities - in progress'),
+                       options = layersControlOptions(collapsed = FALSE)) |> 
+      addPolygons(data = centralWH,
+                  label = ~paste(APN, class),
+                  weight = 1, 
+                  fillOpacity = 0.7,
+                  color= ~palWH(category),
+                  group = 'Industrial Parcels') |> 
+      addLegend(data = centralWH,
+                title = 'Industrial Parcels',
+                pal = palWH,
+                values = ~category,
+                group = 'Industrial Parcels') |> 
+      addPolygons(data = cities2,
+                  label = ~Zone_Label,
                   weight = 1, 
                   fillOpacity = 0.4,
-                  color= ~palJuris2(ZNDESCRIPT),
-                  group = 'SJV County') %>% 
-      addLegend(data = whZ,
-                title = 'SJV County Zoning',
-                pal = palJuris2,
-                values = ~ZNDESCRIPT,
-                group = 'SJV County') %>% 
-      addPolygons(data = tracy_parcels,
-                  label = ~htmlEscape(paste(APN, ZONING_COD)),
-                  weight = 1, 
-                  fillOpacity = 0.4,
-                  color = ~palTracy(ZONING_COD),
-                  group = 'Tracy')  %>% 
-      addLegend(data = tracy_parcels,
-                title = 'Tracy Zoning',
-                pal = palTracy,
-                values = ~ZONING_COD,
-                group = 'Tracy') #%>% 
+                  color = 'lightblue',
+                  group = 'Cities - in progress')  |> 
+      addLegend(data = cities2,
+                labels = 'Cities - in progress',
+                colors = 'lightblue',
+                group = 'Cities - in progress') #|> 
   })
 }
 
